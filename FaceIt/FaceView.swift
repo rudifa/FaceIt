3ce7b5f698f834c08990e51cad48bf66dccd3e0a
rudifa <rudi.farkas@gmail.com>
2018-07-23 20:04:45 +0200

    draw the skull and eyes

--- FaceIt/FaceView.swift
+++ FaceIt/FaceView.swift
@@ -10,14 +10,59 @@ import UIKit
 
 class FaceView: UIView {
 
+    var scale: CGFloat = 0.9
+
+    private var skullRadius: CGFloat {
+        return min(bounds.width, bounds.height) / 2 * scale
+    }
+
+    private var skullCenter: CGPoint {
+        return CGPoint(x: bounds.midX, y: bounds.midY)
+    }
+
+    private enum Eye {
+        case left, right
+    }
+
+    private func pathForEye(_ eye: Eye) -> UIBezierPath {
+        func centerOfEye(_ eye: Eye) -> CGPoint {
+            let eyeOffset = skullRadius / Ratios.skullRadiusToEyeOffset
+            var eyeCenter = skullCenter
+            eyeCenter.y -= eyeOffset
+            eyeCenter.x += ((eye == .left) ? -1 : 1) * eyeOffset
+            return eyeCenter
+        }
+        let eyeRadius = skullRadius / Ratios.skullRadiusToEyeRadius
+        let eyeCenter = centerOfEye(eye)
+        let path = pathForCircle(center: eyeCenter, radius: eyeRadius)
+        path.lineWidth = 5.0
+        return path
+    }
+
+    private func pathForCircle(center: CGPoint, radius: CGFloat)  -> UIBezierPath {
+        return UIBezierPath(arcCenter: center, radius: radius, startAngle: 0, endAngle: CGFloat(2 * Double.pi), clockwise: false)
+    }
+
+    private func pathForSkull() -> UIBezierPath {
+        let path = pathForCircle(center: skullCenter, radius: skullRadius)
+        path.lineWidth = 5.0
+        return path
+    }
+
     override func draw(_ rect: CGRect) {
 
-        let skullRadius = min(bounds.width, bounds.height) / 2
-        let skullCenter = CGPoint(x: bounds.midX, y: bounds.midY)
-        let path = UIBezierPath(arcCenter: skullCenter, radius: skullRadius, startAngle: 0, endAngle: CGFloat(2 * Double.pi), clockwise: false)
-        path.lineWidth = 5.0
         UIColor.blue.set()
-        path.stroke()
+        pathForSkull().stroke()
+        pathForEye(.left).stroke()
+        pathForEye(.right).stroke()
+    }
+
+    private struct Ratios {
+        static let skullRadiusToEyeOffset: CGFloat = 3
+        static let skullRadiusToEyeRadius: CGFloat = 10
+        static let skullRadiusToMouthWidth: CGFloat = 3
+        static let skullRadiusToMouthHeight: CGFloat = 3
+        static let skullRadiusToMouthOffset: CGFloat = 3
     }
 
 }
